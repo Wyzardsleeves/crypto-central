6fb058fbd0c080151afabfd1138429132af907c0
JayLatt <jlattimor3@gmail.com>
2017-09-03 03:06:33 -0400

    basic layout

---
+++ Components/Crypto.js
@@ -0,0 +1,9 @@
+class Crypto extends Component {
+  render(){
+    return (
+      <div className="crypto"> //might need to change back to App
+        <h2>This is a test for Crypto </h1>
+      </div>
+    );
+  }
+}
--- src/App.js
+++ src/App.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Main app
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-header">
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app-body">
+          <Crypto />
+        </div>
+        <div className="app-foot">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+//Build crypto list
+class Crypto extends Component {
+  render(){
+    return (
+      <div className="crypto"> //might need to change back to App
+        <h5>This is a test for Crypto </h5>
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+<div className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <h2>Welcome to React</h2>
+</div>
+<p className="App-intro">
+  To get started, edit <code>src/App.js</code> and save to reload.
+</p>
+*/
