7527a6357c1c0139890219c75cd2685fc99db521
JayLatt <jlattimor3@gmail.com>
2017-09-26 21:03:06 -0400

    fixed all of font-awesome BS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import logo from './cc-home-logo.png'
 import './App.css';
 
 class App extends Component {
@@ -33,27 +34,59 @@ class App extends Component {
       <div className="app">
         <div className="app-head">
           <div className="container">
+            <div className="head-left">
+              <img src={logo} alt="crypto-central" />
+              <h4>Live crypto currency feed!</h4>
+            </div>
+            <div className="head-right">
 
+            </div>
           </div>
         </div>
         <div className="app-body">
           <div className="container">
-            <ul className="crypto-list">
-              {this.state.all.map((coin, index) =>
-                <li key={index}>
-                  <div className="crypto-currencies">
-                    <h5>{coin.symbol}</h5>
-                    <h5>{coin.id}</h5>
-                    <h5>${coin.price_usd}</h5>
-                  </div>
-                </li>
+            <div className="crypto-list">
+            <table>
+              <tr>
+                <th>Index</th>
+                <th>Short</th>
+                <th>Name</th>
+                <th>Price(usd)</th>
+              </tr>
+              {this.state.all.map((coin) =>
+                  <tr className="crypto-currencies">
+                    <td className="col-sm-1">{coin.rank}</td>
+                    <td className="col-sm-2">{coin.symbol}</td>
+                    <td className="col-sm-4">{coin.name}</td>
+                    <td className="col-sm-1">${coin.price_usd}</td>
+                  </tr>
               )}
-            </ul>
+              </table>
+            </div>
           </div>
         </div>
         <div className="app-foot">
           <div className="container">
-
+            <div>
+              <div className="general-info col-xs-2">
+                <h6 href="#">About</h6>
+                <h6 href="#">Services</h6>
+                <h6 href="#">Careers</h6>
+                <h6 href="#">Blog</h6>
+              </div>
+              <div className="sub-info col-xs-2">
+                <h6 href="#">News</h6>
+                <h6 href="#">Site Map</h6>
+                <h6 href="#">Privacy Policy</h6>
+                <h6 href="#">Terms Of Service</h6>
+              </div>
+              <div className="contact-info col-xs-2">
+                <h6>Crypto Central</h6>
+                <h5>(555) 563-2385</h5>
+                <h5>555 Old Phony Ln</h5>
+                <h5>Fakeville, GA</h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -61,17 +94,4 @@ class App extends Component {
   }
 }
 
-/* Original
-getCryptoData(){
-  axios.get('https://coinmarketcap-nexuist.rhcloud.com/api/all')
-    .then((response) => {
-      this.setState({all: response.data}, function(){
-        console.log(this.state.all);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-}
-*/
 export default App;
